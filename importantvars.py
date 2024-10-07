4a258b6a8c8501faa704cc6de95b4aaba3caae19
CanineNote <caninenote@gmail.com>
2024-10-07 16:10:55 -0400

    Update importantvars.py

--- importantvars.py
+++ importantvars.py
@@ -8,5 +8,5 @@ count = 0
 # Count is used to check if the function needs to be re-ran. >3 == Stop running.
 players = 0
 # Counts the total players input in the beginning of Hangman.py
-wordbank = []
+wordbank = ["Apple", "Kachow", "Skateboard", "Mario", "Stupid", "Dog", "Car", "Cat", "Yeti", "Bitcoin", "Coin", "Base", "Computer", "Snowball", "Blender", "Toothbrush", "Hairbrush", "Charcoal", "Wood", "Fire", "Hammer", "Fortnite", "Minecraft", "Steve"]
 # Automatically assigned for singleplayer, but appended to with an input given by player in Hangman.py for multiplayer.
